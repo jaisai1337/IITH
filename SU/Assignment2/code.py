c342128f3f65659a7e180249b274451c07905009
jaisai1337 <48624947+jaisai1337@users.noreply.github.com>
2021-11-16 14:19:38 +0530

    Create code.py

---
+++ SU/Assignment2/code.py
@@ -0,0 +1,55 @@
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+def line_gen(A,B):
+  len =10
+  dim = A.shape[0]
+  x_AB = np.zeros((dim,len))
+  lam_1 = np.linspace(0,1,len)
+  for i in range(len):
+    temp1 = A + lam_1[i]*(B-A)
+    x_AB[:,i]= temp1.T
+  return x_AB
+
+
+
+A =np.array([-2,-1])
+B =np.array([1,0])
+C =np.array([4,3])
+D =np.array([1,2])
+
+
+
+# method 1
+#D=np.linalg.norm(A-B)
+#print("The distance between ",A," and ",B," is: ",D)
+x_AB = line_gen(A,B)
+x_BC = line_gen(B,C)
+x_CD = line_gen(C,D)
+x_DA = line_gen(D,A)
+
+
+
+plt.plot(x_AB[0,:],x_AB[1,:],label='$AB$')
+plt.plot(x_BC[0,:],x_BC[1,:],label='$BC$')
+plt.plot(x_CD[0,:],x_CD[1,:],label='$CD$')
+plt.plot(x_DA[0,:],x_DA[1,:],label='$DA$')
+
+plt.plot(A[0], A[1], 'o')
+plt.text(A[0] * (1 + 0.1), A[1] * (1 - 0.1) , 'A')
+plt.plot(B[0], B[1], 'o')
+plt.text(B[0] * (1-0.3), B[1] * (1-0.1) , 'B')
+plt.plot(C[0], C[1], 'o')
+plt.text(C[0] * (1 + 0.1), C[1] * (1 - 0.1) , 'C')
+plt.plot(D[0], D[1], 'o')
+plt.text(D[0] * (1-0.3), D[1] * (1-0.1) , 'D')
+
+
+
+plt.xlabel('$x$')
+plt.ylabel('$y$')
+plt.legend(loc='best')
+plt.grid() # minor
+plt.axis('equal')
+plt.show()
